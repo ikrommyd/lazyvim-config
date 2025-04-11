de5e19642db8057d40cccf1baa0c95e231540050
Iason Krommydas <iason.krom@gmail.com>
2025-04-11 00:36:27 -0500

    add telescopy

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -23,4 +23,24 @@ return {
   {
     "github/copilot.vim",
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+      },
+    },
+    keys = {
+      {
+        "<leader>/",
+        "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+        desc = "Grep (root dir)",
+      },
+    },
+    config = function(_, opts)
+      local tele = require("telescope")
+      tele.setup(opts)
+      tele.load_extension("live_grep_args")
+    end,
+  },
 }
