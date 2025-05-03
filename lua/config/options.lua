cf808885fa26e5cf6412a761aaeb0531f9eaa617
Iason Krommydas <iason.krom@gmail.com>
2025-05-03 01:25:58 -0500

    configure netrw

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
